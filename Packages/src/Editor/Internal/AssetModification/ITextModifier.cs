caa495a145b2a2e4bdde5fad920f3f2a4fe86fdb
mob-sakai <sakai861104@gmail.com>
2024-01-04 17:47:54 +0900

    chore: update coffee.internal

--- Packages/src/Editor/Internal/AssetModification/ITextModifier.cs
+++ Packages/src/Editor/Internal/AssetModification/ITextModifier.cs
@@ -4,6 +4,6 @@ namespace Coffee.UISoftMask.Internal.AssetModification
 {
     internal interface ITextModifier
     {
-        public bool ModifyText(StringBuilder sb, string text);
+        bool ModifyText(StringBuilder sb, string text);
     }
 }
--- Packages/src/Editor/Internal/ShaderPreProcessorBase.cs
+++ Packages/src/Editor/Internal/ShaderPreProcessorBase.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using UnityEditor.Build;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Coffee.UISoftMask.Internal
 {
@@ -17,7 +18,7 @@ namespace Coffee.UISoftMask.Internal
         {
             Console.WriteLine($"[{shader.name}] {data.Count} variants available:");
             foreach (var (platform, keywords) in data
-                         .Select(d => (d.shaderCompilerPlatform, GetKeyWords(d, ignoredKeywords)))
+                         .Select(d => (d.shaderCompilerPlatform, GetKeyWords(shader, d, ignoredKeywords)))
                          .OrderBy(t => t.Item2))
             {
                 Console.WriteLine($"  - {platform}: {keywords}");
@@ -45,10 +46,14 @@ namespace Coffee.UISoftMask.Internal
             }
         }
 
-        private static string GetKeyWords(ShaderCompilerData data, IEnumerable<string> ignoredKeywords)
+        private static string GetKeyWords(Shader shader, ShaderCompilerData data, IEnumerable<string> ignoredKeywords)
         {
             return string.Join("|", data.shaderKeywordSet.GetShaderKeywords()
+#if UNITY_2021_2_OR_NEWER
                 .Select(x => x.name)
+#else
+                .Select(x => ShaderKeyword.GetKeywordName(shader, x))
+#endif
                 .Where(k => ignoredKeywords == null || !ignoredKeywords.Contains(k))
                 .OrderBy(k => k));
         }
